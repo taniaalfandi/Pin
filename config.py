ce42bccf6497eeb0919bfdf3b167726d51ac4589
taniaalfandi <taniaalfandi@gmail.com>
2025-06-06 16:03:10 +0700

    Update config.py

--- config.py
+++ config.py
@@ -18,32 +18,32 @@ def is_enabled(value, default):
         return default
       
 # Bot Information
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "27342258"))
+API_HASH = environ.get("API_HASH", "df1d220364ecb7ac56d29152d194e14f")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8198025806:AAH3OIpsy2mrT5t5FJ39qVJc2-4suhFp1FQ")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "fileshareinbot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
 CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
-CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://upinputra30:RoOsNOLn8kDv8Vv8@upinputra.lzwpkfr.mongodb.net/?retryWrites=true&w=majority&appName=upinputra")
+CDB_NAME = environ.get("CDB_NAME", "upinputra30")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "techvjbotz")
+DB_URI = environ.get("DB_URI", "mongodb+srv://upinputra30:RoOsNOLn8kDv8Vv8@upinputra.lzwpkfr.mongodb.net/?retryWrites=true&w=majority&appName=upinputra")
+DB_NAME = environ.get("DB_NAME", "upinputra30")
 
 # Auto Delete Information
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
 
 # If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "10")) # Time in Minutes
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "600")) # Time in Seconds
 
 # Channel Information
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
